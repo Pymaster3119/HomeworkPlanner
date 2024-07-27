e220a0aa14e38b2af4dfca5d8e69bad16d80133f
pymaster9 <adi31tya@icloud.com>
2024-07-27 16:19:16 -0400

    Dynamically figuring out the schedules per day

--- DetailedView.py
+++ DetailedView.py
@@ -8,6 +8,7 @@ import calendar
 import math
 import Main
 from tkinter import ttk
+import math
 
 import tkinter as tk
 from tkinter import ttk
@@ -36,11 +37,34 @@ def detailedview(top, widget):
         for col in range(4):
             tempframe.grid_columnconfigure(col, weight=1)
     #Create schedule
-    startWorkTime = 180
     breaktime = 15
+    timeline = []
+    workperiods = 30
     #Working from hardest to easiest so that workload becomes easier as time goes on
     sorted_assignments = sorted(widget.assignments, key=lambda x: x.difficulty, reverse=True)
-    print(sorted_assignments)
+    for assignment in sorted_assignments:
+        numdays = datetime.strptime(assignment.enddate, "%m/%d/%y").date() - datetime.strptime(assignment.startdate, "%m/%d/%y").date()
+        numdays = numdays.days
+        for i in range(math.ceil(int(assignment.time)/numdays/30)):
+            timeline.append((assignment, workperiods))
+            timeline.append(("break", breaktime))
 
-
-    
+    #Display schedule
+    tk.Label(frame, text="Schedule:").grid (row=3,column=0)
+    scheduleframes = Main.ScrollableFrame(frame)
+    scheduleframes.grid(row=4, column=0, sticky="nsew")
+    scheduleframes.scrollable_frame.grid_rowconfigure(0, weight=1)
+    scheduleframes.scrollable_frame.grid_columnconfigure(0, weight=1)
+    currenttime = 180
+    for idx,i in enumerate(timeline):
+        tempframe = ttk.Frame(scheduleframes.scrollable_frame)
+        tempframe.grid(row=idx, column=0, padx = 10, pady=5, sticky="ew")
+        tk.Label(tempframe,text=str(math.floor(currenttime/60)) + ":" + str(currenttime % 60)).grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+        currenttime += i[1]
+        tk.Label(tempframe,text=str(math.floor(currenttime/60)) + ":" + str(currenttime % 60)).grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+        if isinstance(i[0], str):
+            tk.Label(tempframe,text=i[0]).grid(row=0, column=2, padx=5, pady=5, sticky="ew")
+        elif isinstance(i[0], Main.Assignment):
+            tk.Label(tempframe,text=i[0].name).grid(row=0, column=2, padx=5, pady=5, sticky="ew")
+            tk.Label(tempframe,text=i[0].difficulty).grid(row=0, column=3, padx=5, pady=5, sticky="ew")
+        

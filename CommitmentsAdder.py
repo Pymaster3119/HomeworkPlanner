de4e044c08a37227fa7881a638fb031428a366bd
pymaster9 <adi31tya@icloud.com>
2024-07-28 01:12:45 -0400

    Making my HomeworkPlanner take in fixed events

---
+++ CommitmentsAdder.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from tkcalendar import Calendar
+import datetime
+import pickle
+from tkinter import messagebox
+import Main
+
+root = Tk()
+
+#Start Date
+Label(root, text="Starting Date:").pack()
+today = datetime.datetime.today()
+startdate = Calendar(root, year = today.year, month = today.month, day = today.day, weekendforeground = "black", firstweekday = "monday", showothermonthdays = False)
+startdate.pack(pady = 20)
+
+#End Date
+Label(root, text="Ending Date:").pack()
+today = datetime.datetime.today()
+enddate = Calendar(root, year = today.year, month = today.month, day = today.day, weekendforeground = "black", firstweekday = "monday", showothermonthdays = False)
+enddate.pack(pady = 20)
+
+#Name
+Label(root, text='What is the name of your commitment?').pack()
+name = StringVar()
+Entry(root, textvariable=name).pack()
+
+#Reason
+Label(root, text='What is the club/reason you are commiting for?').pack()
+reason = StringVar()
+Entry(root, textvariable=reason).pack()
+
+#Starttime
+Label(root, text='When does it start?:').pack()
+starttime = StringVar()
+Entry(root, textvariable=starttime).pack()
+
+#Length
+Label(root, text='How long is it?:').pack()
+duration = StringVar()
+Entry(root, textvariable=duration).pack()
+
+#Dates
+Label(root, text='What days of the week will this be?:').pack()
+tempframe = Frame(root)
+tempframe.pack()
+weekdayvars = []
+for idx, weekday in enumerate(Main.WEEKDAYS):
+    variable = BooleanVar(tempframe, False)
+    weekdayvars.append(variable)
+    Checkbutton(tempframe, text=weekday, variable=variable).grid(row=0, column=idx)
+    
+
+#Create Commitment
+def saveCommitments():
+    commitmentlist = []
+    with open("commitmentslist", "rb") as listfile:
+        commitmentlist = pickle.load(listfile)
+    commitmentlist.append(Main.Commitment(startdate, enddate, starttime, duration, name, reason, weekdayvars))
+    with open("commitmentslist", "wb") as listfile:
+        pickle.dump(commitmentlist, listfile)
+    messagebox.showinfo(message="Commitment posted!")
+
+
+Button(root, text="Save Commitment", command=saveCommitments).pack()
+
+root.mainloop()
--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import datetime
 import tkinter as tk
 from tkinter import ttk
+import pickle
 
 class Assignment():
     def __init__(self, startdate, enddate, workunits, difficulty, name, subject, time):
@@ -12,7 +13,18 @@ class Assignment():
         self.subject = subject.get()
         self.time = time.get()
 
-# HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP 
+class Commitment:
+    def __init__ (self, startdate, enddate, starttime, duration, name, reason, days):
+        self.startdate = startdate.get_date()
+        self.enddate = enddate.get_date()
+        self.starttime = int(starttime.get())
+        self.duration = int(duration.get())
+        self.name = name.get()
+        self.reason = reason.get()
+        self.days = []
+        for i in days:
+            self.days.append(i.get())
+
 class ScrollableFrame(ttk.Frame):
     def __init__(self, container, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
@@ -68,6 +80,8 @@ COLORS = [
     "#40E0D0"   # Turquoise
 ]
 
+WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
 def get_dates_between(date1_str, date2_str):
     # Convert string dates to datetime.date objects
     date1 = datetime.datetime.strptime(date1_str, "%m/%d/%y").date()
@@ -77,3 +91,9 @@ def get_dates_between(date1_str, date2_str):
     delta = date2 - date1
     all_dates = [date1 + datetime.timedelta(days=i) for i in range(delta.days + 1)]
     return all_dates
+
+if __name__ == "__main__":
+    assignmentlist = []
+    listname = input("What list do you want to remove?: ")
+    with open(listname, "wb") as listfile:
+        pickle.dump(assignmentlist, listfile)
